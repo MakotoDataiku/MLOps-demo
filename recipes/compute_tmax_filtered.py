3ead4f6fa7dfcd1f0c48701f9f21826128e9f910
admin <admin>
2021-08-09 10:19:41 +0200

    Updated recipe WN_MLOps_initial.compute_tmax_filtered

--- recipes/compute_tmax_filtered.py
+++ recipes/compute_tmax_filtered.py
@@ -5,16 +5,10 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 weather_historical_copy = dataiku.Dataset("weather_historical_copy")
-weather_historical_copy_df = weather_historical_copy.get_dataframe()
+df = weather_historical_copy.get_dataframe()
 
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-tmax_filtered_df = weather_historical_copy_df # For this sample code, simply copy input to output
-
+df = df[df['Tmax'] > 70]
 
 # Write recipe outputs
 tmax_filtered = dataiku.Dataset("tmax_filtered")
-tmax_filtered.write_with_schema(tmax_filtered_df)
+tmax_filtered.write_with_schema(df)
